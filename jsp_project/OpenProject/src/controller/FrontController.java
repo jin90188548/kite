0d47f47b89afbf774a213485d21db507ae65632b
유영진 <ryuyj76@naver.com>
2019-12-24 12:01:10 +0900

    Service MAP 생성 처리

--- jsp_project/OpenProject/src/controller/FrontController.java
+++ jsp_project/OpenProject/src/controller/FrontController.java
@@ -1,19 +1,39 @@
 package controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.IndexMemberServiceImpl;
+import service.MemberService;
+import service.RegFormMemberServiceImpl;
+
 public class FrontController extends HttpServlet {
+	
+	private Map<String, MemberService> uriMap = 
+			
+			new HashMap<String, MemberService>();
 
 	// 1. 초기화 : Service 객체들을 관리 -> init() 오버라이딩
 	@Override
 	public void init() throws ServletException {
-		// TODO Auto-generated method stub
-		super.init();
+		
+		// / 요청
+		uriMap.put("/", new IndexMemberServiceImpl());
+		// /index
+		uriMap.put("/index", new IndexMemberServiceImpl());
+		// /member/regForm
+		uriMap.put("/member/regForm", new RegFormMemberServiceImpl());
+		
+		// 기능 확장 : Service 구현 클래스 생성 
+		
+		
 	}
 
 	
@@ -28,7 +48,7 @@ public class FrontController extends HttpServlet {
 		process(req, resp);
 	}
 	
-	private void process(HttpServletRequest request, HttpServletResponse response) {
+	private void process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
 		// 요청 분석
 		String uri = request.getRequestURI();   //    /op/member/list
@@ -38,6 +58,47 @@ public class FrontController extends HttpServlet {
 			uri = uri.substring(conPath.length());
 		}
 		System.out.println("사용자 요청 URI : " + uri);
+		
+		
+		// 요구사항에 대한 처리
+		// 핵심처리, 결과 데이터를 request 속성에 저장
+		// view 페이지 지정
+		
+		MemberService ms = null;
+		
+		ms = uriMap.get(uri);
+		
+		/*
+		 * if(uri.equals("/index") || uri.equals("/")) { ms = new
+		 * IndexMemberServiceImpl(); } else if(uri.equals("/member/regForm")) { ms = new
+		 * RegFormMemberServiceImpl(); }
+		 */
+		
+		String viewPage = "/WEB-INF/views/invalid.jsp"; 
+				
+		if(ms!=null) {
+			viewPage = ms.process(request);
+		}
+				
+				 
+		
+		
+		// 포워딩
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 
