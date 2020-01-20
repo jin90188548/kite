36de410bc23579040fa6a4da95260b848b3dabf6
유영진 <ryuyj76@naver.com>
2020-01-20 15:09:45 +0900

    Update 2020.01.15.sql

--- SQL/2020.01.15.sql
+++ SQL/2020.01.15.sql
@@ -3,7 +3,7 @@ create table project.OPMEMBER (
     uemail VARCHAR(100) unique not null,
     uname VARCHAR(100) unique not null,
     upw varchar(20) not null,
-    gender char(1) check ( gender='m' or gender='w' ) not null,
+    gender char(1) check ( gender='M' or gender='F' ) ,
     byear int,
     uphoto varchar(200),
     regdate datetime
