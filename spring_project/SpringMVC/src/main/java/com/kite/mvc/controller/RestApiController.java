57e834545c24ff7466a8686e8cc3e20a40d31cfb
유영진 <ryuyj76@naver.com>
2020-01-21 15:26:08 +0900

    @CrossOrgin

--- spring_project/SpringMVC/src/main/java/com/kite/mvc/controller/RestApiController.java
+++ spring_project/SpringMVC/src/main/java/com/kite/mvc/controller/RestApiController.java
@@ -1,6 +1,6 @@
 package com.kite.mvc.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +10,7 @@ import com.kite.mvc.domain.MemberVo;
 @RestController
 public class RestApiController {
 
+	@CrossOrigin
 	@PostMapping("/api/members")
 	public String insertMember(@RequestBody MemberVo member) {		
 		return member.toString(); 
