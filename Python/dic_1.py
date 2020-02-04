ea24c32523e14fe7329856c35d1eae510f535edd
유영진 <ryuyj76@naver.com>
2020-02-04 16:01:37 +0900

    딕셔너리

---
+++ Python/dic_1.py
@@ -0,0 +1,32 @@
+# 딕셔너리 선언 { key:value, ....} 
+# key => int , str, bool
+# value => 숫자, 문자열, bool, list, dic
+
+dic_a = {
+    'name':'홍길동',
+    'phoneNumber':'0107779999',
+    'birthday':'20000101'
+}
+
+print('----- dic_a -----')
+print(dic_a)
+
+print('------------------')
+print('이름 : ', dic_a['name'])
+print('전화번호 : ', dic_a['phoneNumber'])
+print('생일 : ', dic_a['birthday'])
+
+
+name = '이름'
+dic_b = {
+    name : '7D 망고저림',
+    'type' : '망고저림'
+}
+
+print(dic_b)
+
+
+
+
+
+print('\n\n\n\n')
