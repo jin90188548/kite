dcf319ebeebf0f820c195dc2cfcabdd54a3af7d7
유영진 <ryuyj76@naver.com>
2020-02-11 12:03:09 +0900

    PhoneBook use class

--- Python/module_basic/phonebook/phonebookmanager.py
+++ Python/module_basic/phonebook/phonebookmanager.py
@@ -3,6 +3,34 @@
 # 데이터를 저장하고 있는 배열(리스트) -> []
 # 기능 메서드 : 입력(리스트), 삭제(리스트 삭제), 검색, 전체 출력
 
+#from phonebook_class import PhoneInfo
+
+# class 정의 : PhoneInfo
+# 속성 : name, phonenumber, birthday
+# 기능 : showInfo
+class PhoneInfo:
+    #생성자
+    def __init__(self, name, phonenumber, birthday):
+        self.name=name
+        self.phonenumber=phonenumber
+        self.birthday=birthday
+    
+    # 정보 출력하는 메서드
+    def showinfo(self):
+        print('--- 정보출력 시작 -------------------')
+        print('이름 : ', self.name, sep='\t')
+        print('전화번호 : ', self.phonenumber, sep='\t')
+        print('생일 : ', self.birthday, sep='\t')
+        print('--- 정보출력 종료 -------------------')
+
+    # 이름 비교후 동일 여부 결과 반환하는 메서드
+    def checkInfo(self, keyword):
+        return self.name==keyword
+
+
+
+
+
 # 친구의 정보를 저장하는 리스트
 pBooks = [] 
 
@@ -11,13 +39,64 @@ def insertMember():
     pNum = input('전화번호를 입력해주세요. >>')
     bDay = input('생일을 입력해주세요. >>')
 
-    member = {
-        'name': name,
-        'phoneNumber' : pNum,
-        'birthday' : bDay
-    }
+    # member = {
+    #     'name': name,
+    #     'phoneNumber' : pNum,
+    #     'birthday' : bDay
+    # }
+
+    # 수정 : 딕셔너리 객체 사용을 class 객체 사용으로 변경
+    # 날짜 : 2020.02.11
+    # 작성자 : jin
+    member = PhoneInfo(name, pNum, bDay)
+
     pBooks.append(member)
 
 def showList():
     for member in pBooks:
-        print(member)
+        #print(member)
+        member.showinfo()
+
+
+def searchInfo():
+
+    print('--- 검색(이름) -------------------')
+    
+    keyword = input('이름을 입력해주세요. >> ')
+
+    chk_num = 0 # 검색결과가 없을때 : 0, 있을때 1 이상
+
+    for member in pBooks:
+        if member.checkInfo(keyword):
+            member.showinfo()
+            chk_num += 1
+    
+    if(chk_num==0):
+        print('찾으시는 이름의 정보가 존재하지 않습니다.')
+
+
+def deleteInfo():
+
+    print('--- 삭제(이름) -------------------')
+    
+    keyword = input('이름을 입력해주세요. >> ')
+
+    # 검색시 위치 확인용
+    searchIndex = 0
+    delCnt = 0
+
+    for member in pBooks:
+        if member.checkInfo(keyword):            
+            del pBooks[searchIndex]
+            delCnt += 1
+        
+        searchIndex += 1
+    
+    if(delCnt==0):
+        print('찾으시는 이름의 정보가 존재하지 않습니다.')
+
+
+
+if __name__=='__main__':
+    pb = PhoneInfo('test','0000','1111')
+    pb.showinfo()
--- Python/phonebook_main.py
+++ Python/phonebook_main.py
@@ -3,7 +3,7 @@ import sys
 
 
 while True:
-    print('메뉴를 입력해주세요.\n (1) 데이터 입력 \n (2) 전체출력 \n (3)exit')
+    print('메뉴를 입력해주세요.\n (1) 데이터 입력 \n (2) 전체출력 \n (3) 검색(이름) \n (4) 삭제 \n (5) exit')
     choice = input('메뉴 번호를 입력해주세요. >>')
 
     if choice=='1':
@@ -11,7 +11,12 @@ while True:
     elif choice=='2':
         pbm.showList()
     elif choice=='3':
+        pbm.searchInfo()
+    elif choice=='4':
+        pbm.deleteInfo()
+    elif choice=='5':
         #break
         sys.exit()
+    
     else :
         print('유요한 메뉴번호가 아닙니다. 확인해주세요')
