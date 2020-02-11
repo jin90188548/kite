528474825dcac24a7ab8701de3c3ed1c00781e83
유영진 <ryuyj76@naver.com>
2020-02-11 17:20:13 +0900

    Update phonebook_main.py

--- Python/phonebook_main.py
+++ Python/phonebook_main.py
@@ -2,6 +2,7 @@ from module_basic.phonebook import phonebookmanager as pbm
 import sys
 
 
+
 while True:
     print('메뉴를 입력해주세요.\n (1) 데이터 입력 \n (2) 전체출력 \n (3) 검색(이름) \n (4) 삭제 \n (5) exit')
     choice = input('메뉴 번호를 입력해주세요. >>')
