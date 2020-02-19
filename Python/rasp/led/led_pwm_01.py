99919e0ee5c46c21095771afd9324a94657dec2e
유영진 <ryuyj76@naver.com>
2020-02-19 10:13:55 +0900

    Update led_pwm_01.py

--- Python/rasp/led/led_pwm_01.py
+++ Python/rasp/led/led_pwm_01.py
@@ -16,15 +16,22 @@ LED = 11
 dc = [0,1,2,3,4,5,6,7,8,9,10,12,13,15,20,30,50,70,100]
 
 # 핀 설정
-GPIO(LED, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
 
 # PWM 객체 생성 : 11번 핀, 주파수 - 100Hz
 p = GPIO.PWM(LED, 100)
 
 # PWM 신호 출력
-p.start(30)
-
+p.start(5)
 time.sleep(5)
+p.start(50)
+time.sleep(5)
+p.start(30)
+time.sleep(5)
+p.start(80)
+time.sleep(5)
+p.start(100)
+
 
 # PWM 정지
 p.stop()
