5a8d57bbb4317454a5035fd0c7597cde93ac2dbb
유영진 <ryuyj76@naver.com>
2020-02-26 11:42:46 +0900

    myApp

---
+++ android/MyApp/app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    View view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+    }
+
+
+
+}
