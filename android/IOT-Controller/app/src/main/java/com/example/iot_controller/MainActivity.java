fe4ffaff5d2c4903d28ba7141858e49deb396831
유영진 <ryuyj76@naver.com>
2020-03-09 17:05:28 +0900

    android Controller

--- android/IOT-Controller/app/src/main/java/com/example/iot_controller/MainActivity.java
+++ android/IOT-Controller/app/src/main/java/com/example/iot_controller/MainActivity.java
@@ -2,13 +2,79 @@ package com.example.iot_controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch sw_red;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sw_red = findViewById(R.id.switch_red);
+
+        sw_red.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    // URL 설정.
+                    String url = "http://192.168.0.64:5000/sw_r";
+                    // AsyncTask를 통해 HttpURLConnection 수행.
+                    NetworkTask networkTask = new NetworkTask(url, null);
+                    networkTask.execute();
+                } else {
+                    // URL 설정.
+                    String url = "http://192.168.0.64:5000/sw_r";
+                    // AsyncTask를 통해 HttpURLConnection 수행.
+                    NetworkTask networkTask = new NetworkTask(url, null);
+                    networkTask.execute();
+                }
+            }
+        });
+
+    }
+
+
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result; // 요청 결과를 저장할 변수.
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+
+            result = requestHttpURLConnection.request_get(url); // 해당 URL로 부터 결과물을 얻어온다.
+
+            return result;
+        }
+
+
+
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            System.out.println(s+"");
+
+
+        }
     }
 }
